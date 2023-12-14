156183d4d6d3bfaeb0a82bc90b2541fb88c85259
hafiedzmada <hafiedz@code.id>
2023-12-14 14:48:00 +0700

    feat: dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,7 @@
 "use client";
+import { motion } from "framer-motion";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -26,18 +29,33 @@ const Navbar = () => {
     },
   ];
 
+  const { theme, setTheme } = useTheme();
   return (
-    <nav className="flex shadow-md px-8 py-2 rounded-2xl gap-x-3 text-sm font-medium">
-      {navLink?.map((link, index) => (
-        <Link
-          href={`${"/" + link.url}`}
-          key={index}
-          className={`${newPathname === link.url && "font-bold"}`}
-        >
-          {link.title}
-        </Link>
-      ))}
-    </nav>
+    <motion.div 
+    initial={{ opacity: 0, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    exit={{ opacity: 0, y: -20 }}
+    transition={{ duration: 0.3 }}
+    className="flex items-center gap-x-4 px-16 pb-4 pt-8">
+      <nav className="flex shadow-md dark:border-gray-500 dark:border-2 px-8 py-2 rounded-2xl gap-x-3 text-sm font-medium">
+        {navLink?.map((link, index) => (
+          <Link
+            href={`${"/" + link.url}`}
+            key={index}
+            className={`${newPathname === link.url && "border-b-2 border-black "}`}
+          >
+            {link.title}
+          </Link>
+        ))}
+      </nav>
+      <motion.button
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+        className="text-center shadow-md dark:shadow-none hover:bg-gray-200 dark:hover:bg-gray-600 transition-all duration-100 text-gray-800 dark:text-white p-2 rounded-full dark:border-2 dark:border-gray-500"
+        whileHover={{ scale: 1.1 }}
+      >
+        {theme === "dark" ? <SunIcon /> : <MoonIcon />}
+      </motion.button>
+    </motion.div>
   );
 };
 
