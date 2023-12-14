1dcc2c292c4aa331acf56b2553bfda14e483f0a7
hafiedzmada <hafiedz@code.id>
2023-12-14 14:48:20 +0700

    feat: resume and projects page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+
 const ResumePage = () => {
   return (
-    <h1>Resume Page</h1>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: "-100%" }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: "100%" }}
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="container mx-auto w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8 px-4"
+      >
+        <h1>Resume Page</h1>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
