f01c0c3d3cb88e8fb7ea0abf70d29bfcf4d60076
hafiedzmada <hafiedz@code.id>
2023-12-14 14:48:58 +0700

    style: fix responsiveness

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,14 +13,14 @@ const AboutPage = () => {
         initial={{ opacity: 0, y: "100%" }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: "100%" }}
-        transition={{ duration: 1, delay: 0.1 }}
-        className="container mx-auto px-4 py-8 w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8"
+        transition={{ duration: 0.5, delay: 0.1 }}
+        className="container mx-auto w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8 px-4"
       >
         <section className="mb-16">
           <h2 className="text-2xl font-semibold">Jobs</h2>
-          <div className="relative m-8">
+          <div className="relative mx-8 my-4">
             <div
-              className="border-r-2 border-gray-500 absolute h-full top-0 mt-2"
+              className="border-r-2 border-gray-500 dark:border-gray-200 absolute h-full top-0 mt-2"
               style={{
                 left: "15px",
               }}
@@ -28,44 +28,44 @@ const AboutPage = () => {
             <ul className="list-none m-0 p-0">
               <li className="mb-4">
                 <div className="flex items-center mb-1">
-                  <div className="bg-gray-500 rounded-full h-4 w-4 ml-2" />
+                  <div className="bg-gray-500 dark:bg-gray-200 rounded-full h-4 w-4 ml-2" />
                   <div className="flex-1 ml-4 font-medium">
                     Frontend Developer
                   </div>
                 </div>
-                <div className="ml-12 text-gray-600">CODE.ID</div>
-                <div className="ml-12 text-gray-500">Mar 2023 - Present</div>
+                <div className="ml-12 text-gray-600 dark:text-gray-300">CODE.ID</div>
+                <div className="ml-12 text-gray-500 dark:text-gray-400">Mar 2023 - Present</div>
               </li>
               <li className="mb-4">
                 <div className="flex items-center mb-1">
-                  <div className="bg-gray-500 rounded-full h-4 w-4 ml-2" />
+                  <div className="bg-gray-500 dark:bg-gray-200 rounded-full h-4 w-4 ml-2" />
                   <div className="flex-1 ml-4 font-medium">
                     Frontend Developer
                   </div>
                 </div>
-                <div className="ml-12 text-gray-600">
+                <div className="ml-12 text-gray-600 dark:text-gray-300">
                   Glints x Kampus Merdeka
                 </div>
-                <div className="ml-12 text-gray-500">Aug 2021 - Feb 2022</div>
+                <div className="ml-12 text-gray-500 dark:text-gray-400">Aug 2021 - Feb 2022</div>
               </li>
               <li className="mb-4">
                 <div className="flex items-center mb-1">
-                  <div className="bg-gray-500 rounded-full h-4 w-4 ml-2" />
+                  <div className="bg-gray-500 dark:bg-gray-200 rounded-full h-4 w-4 ml-2" />
                   <div className="flex-1 ml-4 font-medium">
                     Frontend Developer
                   </div>
                 </div>
-                <div className="ml-12 text-gray-600">Kamar Pelajar</div>
-                <div className="ml-12 text-gray-500">Jan 2021 - Apr 2021</div>
+                <div className="ml-12 text-gray-600 dark:text-gray-300">Kamar Pelajar</div>
+                <div className="ml-12 text-gray-500 dark:text-gray-400">Jan 2021 - Apr 2021</div>
               </li>
             </ul>
           </div>
         </section>
         <section>
           <h2 className="text-2xl font-semibold">Education</h2>
-          <div className="relative m-8">
+          <div className="relative mx-8 my-4">
             <div
-              className="border-r-2 border-gray-500 absolute h-full top-0 mt-2"
+              className="border-r-2 border-gray-500 dark:border-gray-200 absolute h-full top-0 mt-2"
               style={{
                 left: "15px",
               }}
@@ -73,15 +73,15 @@ const AboutPage = () => {
             <ul className="list-none m-0 p-0">
               <li className="mb-4">
                 <div className="flex items-center mb-1">
-                  <div className="bg-gray-500 rounded-full h-4 w-4 ml-2" />
+                  <div className="bg-gray-500 dark:bg-gray-200 rounded-full h-4 w-4 ml-2" />
                   <div className="flex-1 ml-4 font-medium">
                     Associate Degree of Information System
                   </div>
                 </div>
-                <div className="ml-12 text-gray-600">
+                <div className="ml-12 text-gray-600 dark:text-gray-300">
                   Universitas Bina Sarana Informatika
                 </div>
-                <div className="ml-12 text-gray-500">
+                <div className="ml-12 text-gray-500 dark:text-gray-400">
                   Graduated: 2022 GPA: 3.91
                 </div>
               </li>
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,63 @@
 "use client";
 import { TypeAnimation } from "react-type-animation";
 import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    // setShow(true);
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0, y: "100%" }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: "100%" }}
-        transition={{ duration: 1, delay: 0.1 }}
-        className="w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8"
-      >
-        <h1 className="text-2xl lg:text-3xl font-medium">Hi!👋 </h1>
-        <p className="text-4xl lg:text-5xl font-bold">
-          I&apos;m Hafiedz Mada M
-        </p>
-        <TypeAnimation
-          sequence={[
-            "Software Engineer",
-            1000,
-            "Frontend Developre",
-            "Frontend Developer",
-            1000,
-            "Lifelong Learner",
-            1000,
-            "Ketoprak Eater",
-            1000,
-            "Dota 2 Offlaner",
-            1000,
-          ]}
-          wrapper="span"
-          speed={50}
-          repeat={Infinity}
-          className="text-xl md:text-2xl lg:text-3xl font-medium text-gray-800"
-        />
-        <p className="text-justify text-gray-600">
-          An experienced Frontend Developer with a year of hands-on expertise in
-          building captivating user interfaces. My primary focus is on{" "}
-          <span className="font-bold text-black">React, Vue and Next.js</span>,
-          where I&apos;ve honed my skills in creating high-performance web
-          applications. I&apos;m passionate about crafting seamless digital
-          experiences and always eager to explore new opportunities in the
-          frontend development realm.
-        </p>
-      </motion.div>
+      {show && (
+        <motion.div
+          initial={{ opacity: 0, y: "100%" }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: "100%" }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+          className="w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8 px-4"
+        >
+          <h1 className="text-2xl lg:text-3xl font-medium">Hi!</h1>
+          <p className="text-4xl lg:text-5xl font-bold">
+            I&apos;m Hafiedz Mada M
+          </p>
+          <TypeAnimation
+            sequence={[
+              "Software Engineer",
+              1000,
+              "Frontend Developre",
+              "Frontend Developer",
+              1000,
+              "Lifelong Learner",
+              1000,
+              "Ketoprak Eater",
+              1000,
+              "Dota 2 Offlaner",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+            className="text-xl md:text-2xl lg:text-3xl font-medium text-gray-800 dark:text-gray-200"
+          />
+          <p className="text-justify text-gray-600 dark:text-gray-300">
+            An experienced Frontend Developer based in Indonesia🇮🇩 with
+            a year of hands-on expertise in building captivating user
+            interfaces. My primary focus is on{" "}
+            <span className="font-bold text-black dark:text-white">
+              React, Vue and Next.js
+            </span>
+            , where I&apos;ve honed my skills in creating high-performance web
+            applications. I&apos;m passionate about crafting seamless digital
+            experiences and always eager to explore new opportunities in the
+            frontend development realm.
+          </p>
+        </motion.div>
+      )}
     </AnimatePresence>
   );
 }
