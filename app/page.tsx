7c122ee48a34a8ac6c25da34b57e1ff537abed3c
hafiedzmada <hafiedz@code.id>
2023-12-18 23:20:29 +0700

    feat: add social media

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@
 import { TypeAnimation } from "react-type-animation";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import {
+  GitHubLogoIcon,
+  InstagramLogoIcon,
+  LinkedInLogoIcon,
+  TwitterLogoIcon,
+} from "@radix-ui/react-icons";
+import Link from "next/link";
 
 export default function Home() {
   const [show, setShow] = useState(true);
@@ -41,9 +48,9 @@ export default function Home() {
             className="text-xl md:text-2xl lg:text-3xl font-medium text-gray-800 dark:text-gray-200"
           />
           <p className="text-justify text-gray-600 dark:text-gray-300">
-            An experienced Frontend Developer based in Indonesia🇮🇩 with
-            a year of hands-on expertise in building captivating user
-            interfaces. My primary focus is on{" "}
+            An experienced Frontend Developer based in Indonesia🇮🇩 with a year
+            of hands-on expertise in building captivating user interfaces. My
+            primary focus is on{" "}
             <span className="font-bold text-black dark:text-white">
               React, Vue and Next.js
             </span>
@@ -52,6 +59,21 @@ export default function Home() {
             experiences and always eager to explore new opportunities in the
             frontend development realm.
           </p>
+          <h1 className="font-semibold text-gray-800 dark:text-gray-100">Let&apos;s Connect!👇</h1>
+          <div className="flex gap-x-2">
+            <Link href="https://linkedin.com/in/hafiedzmada" target="_blank">
+              <LinkedInLogoIcon className="w-6 h-6 hover:text-gray-700 dark:hover:text-gray-400" />
+            </Link>
+            <Link href="https://github.com/ketoprac" target="_blank">
+              <GitHubLogoIcon className="w-6 h-6 hover:text-gray-700 dark:hover:text-gray-400" />
+            </Link>
+            <Link href="https://instagram.com/hafiedzmada" target="_blank">
+              <InstagramLogoIcon className="w-6 h-6 hover:text-gray-700 dark:hover:text-gray-400" />
+            </Link>
+            <Link href="https://twitter.com/hafiedzmada" target="_blank">
+              <TwitterLogoIcon className="w-6 h-6 hover:text-gray-700 dark:hover:text-gray-400" />
+            </Link>
+          </div>
         </motion.div>
       )}
     </AnimatePresence>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,7 +11,14 @@ const ProjectsPage = () => {
         transition={{ duration: 0.5, delay: 0.1 }}
         className="container mx-auto w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8 px-4"
       >
-        <h1>Featured Projects</h1>
+        <h1 className="font-semibold text-2xl lg:text-3xl mb-2">Featured Projects</h1>
+        <div>
+          <p>Featured Projects</p>
+        </div>
+        <h1 className="font-semibold text-2xl lg:text-3xl mb-2">Projects</h1>
+        <div>
+          <p>Mini Projects</p>
+        </div>
       </motion.div>
     </AnimatePresence>
   );
