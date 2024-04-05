2c6212a151a7ef7bc5af70d365ebe2ea7832ecde
hafiedzmada <hafiedz@code.id>
2024-04-05 19:39:01 +0700

    feat: improve ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Providers from "@/providers/Providers";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
+        <NextTopLoader height={4} color="#000" showSpinner={false} />
           <main className="flex min-h-screen flex-col items-center">
             <Navbar />
             {children}
--- app/template.tsx
+++ app/template.tsx
@@ -4,10 +4,10 @@ import { motion } from "framer-motion";
 const Template = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: "80%" }}
-      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 0, scale: 0.5 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, y: "100%" }}
-      transition={{ duration: 1, delay: 0.1 }}
+      transition={{ duration: 0.4, delay: 0.5 }}
       className="container mx-auto w-full md:w-8/12 lg:w-1/2 flex flex-col gap-y-2 mt-8 px-4"
     >
       {children}
